24086a57da05cadf0dd079a8a862e8c1dbb044b4
JaZahn <jcleveng@fas.harvard.edu>
2015-12-02 13:15:44 -0500

    adding a simple gulpfile

--- media_manager/app/gulpfile.js
+++ media_manager/app/gulpfile.js
@@ -1,2 +1,49 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+var Server = require('karma').Server;
+var jshint = require('gulp-jshint');
+var connect = require('gulp-connect');
+
+gulp.task('moveHTML', function(){
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('buildJS', function(){
+  return gulp.src('src/js/app.js')
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('buildCSS', function(){
+  return gulp.src('src/css/**/*.css')
+    .pipe(concat('styles.css'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('build/css'));
+})
+
+gulp.task('buildVendor', function(){
+  return gulp.src('bower_components/**/*.min.js')
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['moveHTML', 'buildJS', 'buildCSS', 'buildVendor']);
+
+gulp.task('connect', function(){
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/js/**/*.js', ['buildJS']);
+  gulp.watch('src/css/**/*.css', ['buildCSS']);
+  gulp.watch('src/**/*.html', ['moveHTML']);
+});
+
+gulp.task('default', ['build', 'watch', 'connect']);
