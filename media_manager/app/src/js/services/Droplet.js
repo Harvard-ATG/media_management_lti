3e42418d2478e8e520880e1fc94288c9d87560b3
JaZahn <jcleveng@fas.harvard.edu>
2016-01-04 22:07:46 -0500

    changing link to the course manager api

--- media_manager/app/src/js/services/Droplet.js
+++ media_manager/app/src/js/services/Droplet.js
@@ -8,25 +8,14 @@ angular.module('media_manager')
   ds.error = false;
   ds.scope = undefined;
 
-  var dummydata = [
-    "http://localhost:7000/image1.jpg",
-    "http://localhost:7000/image2.jpg"
-  ];
 
   // Listen for when the interface has been configured.
   ds.whenDropletReady = function() {
 
       ds.interface.allowedExtensions(['png', 'jpg', 'bmp', 'gif', 'svg', 'torrent']);
-      ds.interface.setRequestUrl('http://localhost:7000/upload.html');
+      ds.interface.setRequestUrl('http://localhost:8000/courses/1/images');
       ds.interface.defineHTTPSuccess([/2.{2}/]);
       ds.interface.useArray(false);
-
-      dummydata.forEach(function(url){
-        addFile(url, function(blob){
-          ds.interface.addFile(blob);
-          ds.scope.$digest();
-        });
-      });
 
   };
 
@@ -40,6 +29,8 @@ angular.module('media_manager')
           ds.scope.success = false;
       }, 5000);
 
+      console.log("done uploading");
+
   };
 
   // Listen for when the files have failed to upload.
@@ -51,30 +42,7 @@ angular.module('media_manager')
           ds.scope.error = false;
       }, 5000);
 
-  };
-
-  var addFile = function(url, callback){
-    var blob = null;
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.responseType = "blob";//force the HTTP response, response-type header to be blob
-    xhr.onload = function()
-    {
-        blob = xhr.response;//xhr.response is now a blob object
-        //var file = FileReader.readAsDataURL(blob);
-        blob.lastModifiedDate = new Date();
-        blob.name = "somename.jpg";
-        //blob.imageData = FileReader.readAsBinaryString(blob);
-                       //FileReader.readAsBinaryString
-        //console.log("data url");
-        //console.log(blob.imageData);
-
-        //console.log(typeof file);
-        //console.log($scope.interface);
-        //$scope.models.push(blob);
-        callback(blob);
-    }
-    xhr.send();
+      console.log("error!");
 
   };
 
