47b180d02dcb23eb2dbc45e6fb1b67590c0d66b3
JaZahn <jcleveng@fas.harvard.edu>
2016-01-05 13:56:24 -0500

    making the collection title editable

--- media_manager/app/src/js/app.js
+++ media_manager/app/src/js/app.js
@@ -1,4 +1,4 @@
-angular.module('media_manager', ['ngRoute', 'ngDroplet', 'filereader', 'ngResource'])
+angular.module('media_manager', ['ngRoute', 'ngDroplet', 'xeditable', 'filereader', 'ngResource'])
 .config(['$routeProvider', function($routeProvider){
   $routeProvider
   .when('/', {
