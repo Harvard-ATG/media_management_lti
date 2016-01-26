0ff2a9474d974140ceac089286a5ab912d4e7a46
Arthur Barrett <abarrett@fas.harvard.edu>
2016-01-26 11:17:54 -0500

    Adding required STATIC_ROOT django setting.

--- media_management_lti/settings/base.py
+++ media_management_lti/settings/base.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'app/build'))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'app/build'))
 STATIC_URL = '/build/'
 
 STATICFILES_DIRS = [
