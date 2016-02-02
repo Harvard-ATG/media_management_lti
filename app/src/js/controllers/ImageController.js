3cee10f896bf6ea2edd6da6f59175efb67ae9337
JaZahn <jcleveng@fas.harvard.edu>
2016-02-02 13:51:17 -0500

    adding breadcrumbs

--- app/src/js/controllers/ImageController.js
+++ app/src/js/controllers/ImageController.js
@@ -1,5 +1,5 @@
 angular.module('media_manager')
-.controller('ImageController', ['$routeParams', 'CourseCache', 'ImageBehavior', function($routeParams, CourseCache, ImageBehavior){
+.controller('ImageController', ['$routeParams', 'CourseCache', 'ImageBehavior', 'Breadcrumbs', '$location', '$scope', function($routeParams, CourseCache, ImageBehavior, Breadcrumbs, $location, $scope){
   var ic = this;
 
   ic.imageBehavior = ImageBehavior;
@@ -13,11 +13,27 @@ angular.module('media_manager')
     }
   });
 
+  var crumbed = false;
+  var resetBreadcrumb = function(){
+    if(crumbed){
+      Breadcrumbs.popCrumb();
+    }
+    Breadcrumbs.addCrumb("Image " + CourseCache.current_image.id, $location.url());
+    crumbed = true;
+  }
+
+  $scope.$watch(function watch(scope){
+    return CourseCache.current_image;
+  }, function handleChange(newval, oldval){
+    resetBreadcrumb();
+  });
+
   ic.next = function(){
     if(ic.index + 1 < CourseCache.images.length){
       ic.index++;
       ic.image = CourseCache.images[ic.index];
       CourseCache.current_image = CourseCache.images[ic.index];
+      resetBreadcrumb();
     }
   };
 
@@ -26,6 +42,7 @@ angular.module('media_manager')
       ic.index--;
       ic.image = CourseCache.images[ic.index];
       CourseCache.current_image = CourseCache.images[ic.index];
+      resetBreadcrumb();
     }
   }
 
