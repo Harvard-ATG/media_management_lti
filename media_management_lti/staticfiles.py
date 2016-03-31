b24287206bd1beccc482d3e7395167f8f8db8d40
Arthur Barrett <abarrett@fas.harvard.edu>
2016-03-31 12:41:35 -0400

    Updated storage class.

--- media_management_lti/staticfiles.py
+++ media_management_lti/staticfiles.py
@@ -15,44 +15,32 @@ class StaticFilesStorage(storage.StaticFilesStorage):
         """
         Post process the given OrderedDict of files (called from collectstatic).
         Processing is actually two separate operations:
-        1. renaming files to include a hash of their content for cache-busting,
-           and copying those files to the target storage.
-        2. adjusting files which contain references to other files so they
-           refer to the cache-busting filenames.
-        If either of these are performed on a file, then that file is considered
-        post-processed.
+        1. Install required dependencies for build.
+        2. Build the JS and CSS and copy to their destinations
         """
         if dry_run:
-            yield
+            return []
 
         NPM_INSTALL = 'npm install --production'
         BOWER_INSTALL = 'bower install'
         GULP_BUILD = 'gulp build'
-
         APP_CWD = os.path.join(settings.BASE_DIR, 'app')
-        print APP_CWD
+        BUILD_SRC = os.path.join(settings.BASE_DIR, 'app', 'build')
+        BUILD_DST = os.path.join(settings.STATIC_ROOT, 'app', 'build')
 
         popen_args = {
             'cwd': APP_CWD,
             'shell': True,
         }
         for cmd in (NPM_INSTALL, BOWER_INSTALL, GULP_BUILD):
-            print "running cmd %s with args %s" % (cmd, popen_args)
+            print "Running cmd [%s] with args [%s]" % (cmd, popen_args)
             child = subprocess.Popen([cmd], **popen_args)
             child.wait()
             if child.returncode != 0:
                 raise Exception("Command failed [%s] with return code [%s]" % (cmd, child.returncode))
 
-        # sort the files by the directory level
-        def path_level(name):
-            return len(name.split(os.sep))
-
-        for name in sorted(paths.keys(), key=path_level, reverse=True):
-            print name
-            yield name, name, False
-
-        build_dst = os.path.join(settings.STATIC_ROOT, 'app', 'build')
-        build_src = os.path.join(settings.BASE_DIR, 'app', 'build')
-        if os.path.exists(build_dst):
-            shutil.rmtree(build_dst)
-        shutil.copytree(build_src, build_dst)
+        if os.path.exists(BUILD_DST):
+            shutil.rmtree(BUILD_DST)
+        shutil.copytree(BUILD_SRC, BUILD_DST)
+        
+        return []
