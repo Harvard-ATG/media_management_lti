758449c0c9db70b5828160e44112c841fa304b44
Arthur Barrett <abarrett@fas.harvard.edu>
2016-03-31 16:58:09 -0400

    Fixed vagrant puppet config to install node and npm. Also fixed issue with bower command when running collecstatic (missing env PATH entry).

--- media_management_lti/staticfiles.py
+++ media_management_lti/staticfiles.py
@@ -21,27 +21,38 @@ class StaticFilesStorage(storage.StaticFilesStorage):
         if dry_run:
             return []
 
+        # Setup commands to run
         NPM_INSTALL = 'npm install --production'
         BOWER_INSTALL = 'bower install'
         GULP_BUILD = 'gulp build'
-        APP_CWD = os.path.join(settings.BASE_DIR, 'app')
+        COMMANDS = (NPM_INSTALL, BOWER_INSTALL, GULP_BUILD)
+
+        # Setup directory paths
+        NODE_MODULES_DIR = os.path.join(settings.BASE_DIR, 'app', 'node_modules')
+        NODE_MODULES_BIN_DIR = os.path.join(NODE_MODULES_DIR, '.bin')
+        APP_BASE_DIR = os.path.join(settings.BASE_DIR, 'app')
         BUILD_SRC = os.path.join(settings.BASE_DIR, 'app', 'build')
         BUILD_DST = os.path.join(settings.STATIC_ROOT, 'app', 'build')
-
+        
+        # Setup args to pass to the commands
         popen_args = { 
-            'cwd': APP_CWD, 
-            'shell': True, 
-        } 
-
-        # Install the dependencies and build the JS
-        for cmd in (NPM_INSTALL, BOWER_INSTALL, GULP_BUILD):
+            'cwd': APP_BASE_DIR, 
+            'shell': True,
+            'env': {
+                'PATH': os.environ['PATH'] +':{0}'.format(NODE_MODULES_BIN_DIR) # so the shell can find the "bower" command
+            }
+        }
+        print "popen_args=%s" % popen_args
+        
+        # Run the commands.
+        for cmd in COMMANDS:
             print "Running cmd [%s] with args [%s]" % (cmd, popen_args)
             child = subprocess.Popen([cmd], **popen_args)
             child.wait()
             if child.returncode != 0:
                 raise Exception("Command failed [%s] with return code [%s]" % (cmd, child.returncode))
 
-        # Copy the build files to the STATIC_ROOT 
+        # Copy the build file artifacts to the STATIC_ROOT so that django can serve them up
         if os.path.exists(BUILD_DST):
             shutil.rmtree(BUILD_DST)
         shutil.copytree(BUILD_SRC, BUILD_DST)
