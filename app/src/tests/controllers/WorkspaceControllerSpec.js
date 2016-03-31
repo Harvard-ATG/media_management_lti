30409069abe1ec634b5c7068fced404b517ebad6
JaZahn <jcleveng@fas.harvard.edu>
2016-03-31 16:52:33 -0400

    adding a couple of dummy tests

---
+++ app/src/tests/controllers/WorkspaceControllerSpec.js
@@ -0,0 +1,14 @@
+describe("WorkspaceController", function(){
+  var workspaceController;
+
+
+  describe("layout", function(){
+    describe("add new image button", function(){
+      it("should expand the size of the library", function(){
+        expect(workspaceController).toBeUndefined();
+
+      });
+
+    });
+  });
+});
