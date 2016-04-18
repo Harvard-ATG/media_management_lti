40a6d067e47ecba4375ab8b33b7ce67c9ba11f3b
Arthur Barrett <abarrett@fas.harvard.edu>
2016-04-18 18:26:27 -0400

    Fixed droplet thumb image src attribute.

--- app/src/js/directives/dropletThumb.js
+++ app/src/js/directives/dropletThumb.js
@@ -6,7 +6,7 @@ angular.module('media_manager')
     },
     restrict: 'EA',
     replace: true,
-    template: '<img style="background-image: url({{ image.thumb_url || image.image_url }})" class="droplet-preview" />',
+    template: '<img src="{{ image.thumb_url || image.image_url }}" class="droplet-preview" />',
 
   };
 }]);
