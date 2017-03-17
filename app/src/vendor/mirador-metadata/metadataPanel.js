72786148e129d3b3536bb5f65202599e1aff78ea
Arthur Barrett <abarrett@fas.harvard.edu>
2017-03-17 13:43:53 -0400

    Re-styled the metadata rendered in Mirador's side panel using the markup/css provided by @Vittorio2015.

--- app/src/vendor/mirador-metadata/metadataPanel.js
+++ app/src/vendor/mirador-metadata/metadataPanel.js
@@ -1,5 +1,30 @@
-(function(M) {
+(function(M, Handlebars) {
 
+  // Defines the width of the sidepanel
+  var SIDEPANEL_WIDTH = 400;
+
+  // Overriding this Mirador method in order to change the hard-coded width
+  // of the side panel (hard coded to 280px). Requires the Mirador library
+  // to already be loaded.
+  //
+  // See mirador source here:
+  // https://github.com/ProjectMirador/mirador/blob/2.3.0/js/src/workspaces/window.js#L567
+  var sidePanelVisibility = M.Window.prototype.sidePanelVisibility;
+  M.Window.prototype.sidePanelVisibility = function(visible) {
+    sidePanelVisibility.apply(this, arguments);
+    console.log("sidepanel mods", visible);
+    var sidePanelElement = this.element.find('.sidePanel');
+    var viewContainerElement = this.element.find('.view-container');
+    if(visible) {
+      sidePanelElement.width(SIDEPANEL_WIDTH);
+      viewContainerElement.css("margin-left", (SIDEPANEL_WIDTH + 20) + "px");
+    }
+  };
+
+  // Adds the plugin to the Mirador global object
+  // Usage:
+  //    var mirador = Mirador(miradorSettings)
+  //    mirador.metadataPlugin();
   M.prototype.metadataPlugin = function(){
     var mirador = this;
 
@@ -19,25 +44,37 @@
         }, {});
       });
 
+
       mirador.eventEmitter.subscribe('currentCanvasIDUpdated.' + windowId, function(event, data){
         var imageId = data;
-
-        var $sidePanel = $('.sidePanel .tabContentArea');
         var html = '';
-        if(metadatas[imageId]){
-          html = '<b>' + metadatas[imageId].label + '</b><br/>';
-          html += metadatas[imageId].datas.map(function(md){
-            var value = md.value || "No Value";
-            var label = md.label || "";
-            return label + ': ' + value;
-          }).join('<br/>');
-          $sidePanel.html(html);
-        }
+        var $sidePanel = $('.sidePanel');
+        var $tabContent = $sidePanel.find(".tabContentArea").css("width", "calc(100% - 10px)");
 
+        if(metadatas[imageId]){
+          template_context = {
+            "name": metadatas[imageId].label,
+            "metadata": metadatas[imageId].datas.map(function(md){
+              var value = md.value || "No Value";
+              var label = md.label || "";
+              return {"value": value, "label":label};
+            })
+          };
+          html = metadata_template(template_context);
+          $tabContent.html(html);
+        }
       });
     });
-
-
   };
 
-})(Mirador);
+  // Template used to render the metadata in the panel
+  var metadata_template = Handlebars.compile([
+    '<table class="metadata">',
+      '<tr><td class="label">Image name</td><td class="information">{{name}}</td></tr>',
+      '{{#each metadata}}',
+        '<tr><td class="label">{{label}}</td><td class="information">{{value}}</td></tr>',
+      '{{/each}}',
+    '</table>'
+  ].join(''));
+
+})(Mirador, Handlebars);
