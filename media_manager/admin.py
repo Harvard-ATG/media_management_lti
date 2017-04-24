0c69403f5a8af0232cc5ca355fbc3734d32bfe32
Arthur Barrett <abarrett@fas.harvard.edu>
2017-04-24 16:23:15 -0400

    Added model to admin panel.

--- media_manager/admin.py
+++ media_manager/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, CourseModule
 
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('id', 'api_course_id', 'lti_context_id', 'lti_tool_consumer_instance_guid', 'created', 'updated')
 
-admin.site.register(Course, CourseAdmin)
+class CourseModuleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'course', 'lti_resource_link_id', 'api_collection_id', 'created', 'updated')
 
+admin.site.register(Course, CourseAdmin)
+admin.site.register(CourseModule, CourseModuleAdmin)
